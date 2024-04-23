e4f57dffc3a04dfd4805b944593811f8fb305188
Emil Pedersen <steamman0103@gmail.com>
2024-04-23 22:16:53 +0200

    Revert "SDL2 with cmake changes so that we should be gui time"
    This reverts commit 03bce00756a8b6a6827a7fa296497ea769aaae76.

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <SDL.h>
 
 #include "Terminal.c"
 
@@ -101,12 +100,7 @@ void GameLoop(char str2[4]) {
 
 
 
-int main(int argc, char* argv[]) {
-
-    SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Quit();
-
-
+int main() {
     //const char *Filename = "C:\\Users\\steam\\CLionProjects\\untitled1\\Cards.txt";
     printf("\tC1\tC2\tC3\tC4\tC5\tC6\tC7\n");
 
