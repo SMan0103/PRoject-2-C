03bce00756a8b6a6827a7fa296497ea769aaae76
Emil Pedersen <steamman0103@gmail.com>
2024-04-23 21:55:12 +0200

    SDL2 with cmake changes so that we should be gui time

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <SDL.h>
 
 #include "Terminal.c"
 
@@ -100,7 +101,12 @@ void GameLoop(char str2[4]) {
 
 
 
-int main() {
+int main(int argc, char* argv[]) {
+
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Quit();
+
+
     //const char *Filename = "C:\\Users\\steam\\CLionProjects\\untitled1\\Cards.txt";
     printf("\tC1\tC2\tC3\tC4\tC5\tC6\tC7\n");
 
